9919060f23da440c246e40bde42548ddae511c75
LAPTOP-OR3C9S0B\ramon <ramonddn@gmail.com>
2024-09-15 19:52:08 +0100

    feat: Start configurating auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { GoogleAuthGuard } from './guards/google-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('google')
+  async googleLogin() {
+    // O Passport redirecionará automaticamente o usuário para o Google
+  }
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/callback')
+  async googleCallback(@Request() req) {
+    // Retorna os dados do usuário após autenticação com o Google
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
